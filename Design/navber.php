75609cd9be6f99981bdbc17a7ed64b1d36326d99
Bappy Chandra Debnath <bappydebath2001@gmail.com>
2025-02-07 01:10:38 +0600

    added all resources

---
+++ Design/navber.php
@@ -0,0 +1,37 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <!-- -->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" contant = "width=device-width,initial-scale=1.0">
+        <title> Onlinr Shop</title>
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
+        <link rel="stylesheet" href="CSS/style.css">
+    </head>
+    <body>
+        <script scr="script.js">
+
+        </script>
+       
+        <!-- Navbar start-->
+      <section id="header">
+        <h4>Fasion World</h4>
+        <!-- <a href ="#"><img src="images/logo.png" class="logo" alt=" "> </a>-->
+        <div>
+            <ul id="navber">
+                <li><a class="active"  href="index.php">Home</a></li>
+                <li><a  href="shop.php">Shop</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="#"><i class="fal fa-shopping-cart"> </i>></i></a></li>
+            </ul>
+        </div>
+      </section>
+
+    </body>
+</html>
